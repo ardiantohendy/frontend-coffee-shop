40e65cb2d9b2ce53bc91e6637d619c113e74df93
Ardiantohendy <bayuardianto421@gmail.com>
2025-04-20 16:20:06 +0700

    working on landingpage

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,54 @@
+import "../style/LandingPage.css";
+
+const LandingPage = () => {
+  return (
+    <>
+      <div className="landingPage">
+        <div className="top">
+          <header>
+            <div className="logo">
+              <p>CoffeePaste.</p>
+            </div>
+            <div className="headMenu">
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Menu</a>
+                </li>
+                <li>
+                  <a href="#">Book</a>
+                </li>
+              </ul>
+            </div>
+          </header>
+          <div className="content">
+            <div className="title">
+              <p>CoffeePaste.</p>
+            </div>
+            <div className="desc">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores velit ipsam eligendi, minima explicabo ab nesciunt officia repellendus saepe voluptatibus excepturi molestias atque commodi quam. Lorem ipsum, dolor sit
+                amet consectetur adipisicing elit. Sed ipsum doloribus aliquid vero quibusdam accusantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, molestias.
+              </p>
+            </div>
+            <div className="btn">
+              <li>
+                <a href="#">Menu</a>
+              </li>
+              <li>
+                <a href="#">Book</a>
+              </li>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
