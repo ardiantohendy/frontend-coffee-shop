cceeb735ee1697ec278ca9bb923b247534321163
Ardiantohendy <bayuardianto421@gmail.com>
2025-04-24 23:36:28 +0700

    add login page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,11 +1,35 @@
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+// import { useState } from "react";
+import "../style/LoginPage.css";
 
 const LoginPage = () => {
-  // const navigate = useNavigate()
+  const navigate = useNavigate();
+  //   const [user, setUser] = useState({ username: "", password: "" });
+
   return (
     <>
       <div className="loginPage">
-        <h1>LOGIN PAGE</h1>
+        <div className="container">
+          <p className="title">Welcome to CoffeePaste.</p>
+          <p className="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa delectus molestias explicabo enim sed distinctio voluptatibus inventore facere alias iusto.</p>
+          <form action="">
+            <div className="username">
+              {/* <label>Username:</label> */}
+              <input type="text" name="username" placeholder="Username" />
+            </div>
+            <div className="password">
+              {/* <label>Password:</label> */}
+              <input type="text" name="password" placeholder="Password" />
+            </div>
+            <div className="buttons">
+              <button type="submit">Log in</button>
+              <button onClick={() => navigate("/")}>Log in as a guest</button>
+            </div>
+          </form>
+          <p className="register">
+            Do not have any account yet? <a href="#">register</a>
+          </p>
+        </div>
       </div>
     </>
   );
---
+++ src/pages/Main.jsx
@@ -0,0 +1,7 @@
+import MainPage from "../components/MainPage";
+
+const Main = () => {
+  return <MainPage />;
+};
+
+export default Main;
