c36c972ff1b673866b176750c62efb3f0e607ce5
Ardiantohendy <bayuardianto421@gmail.com>
2025-04-28 12:28:50 +0700

    re styling web page

--- src/components/MenuPage.jsx
+++ src/components/MenuPage.jsx
@@ -32,19 +32,21 @@ const MenuPage = () => {
             </button>
           ))}
         </div>
-        <div className="menuList">
-          {menuList
-            .filter((item) => item.category === activeCategory)
-            .map((item) => (
-              <div key={item.id} className="menuItem" onClick={() => setSelectedItem(item)}>
-                <img src={item.image} alt={item.name} className="menuImage" />
-                <div className="menuText">
-                  <p className="title">{item.name}</p>
-                  <p className="desc">{item.description}</p>
-                  <p className="price">Rp {item.price}</p>
+        <div className="outerMenu">
+          <div className="menuList">
+            {menuList
+              .filter((item) => item.category === activeCategory)
+              .map((item) => (
+                <div key={item.id} className="menuItem" onClick={() => setSelectedItem(item)}>
+                  <img src={item.image} alt={item.name} className="menuImage" />
+                  <div className="menuText">
+                    <p className="title">{item.name}</p>
+                    <p className="desc">{item.description}</p>
+                    <p className="price">Rp {item.price}</p>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+          </div>
         </div>
 
         {selectedItem && (
